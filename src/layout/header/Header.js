d4ed6b3c681232938aff7da5ba0041a6123afc1b
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-12-14 10:57:12 +0630

    added header for large device and medium device

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,13 +1,148 @@
-import React from 'react'
-import useStyles from './styles'
+import React from "react";
+import ApartmentIcon from "@material-ui/icons/Apartment";
+import PhoneIcon from "@material-ui/icons/Phone";
+import EmailIcon from "@material-ui/icons/Email";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import AddIcon from "@material-ui/icons/Add";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from '@material-ui/icons/Close';
+import useStyles from "./styles";
+import HeaderLogo from "../../images/header-logo.jpg";
 
 const Header = () => {
     const classes = useStyles();
-    return (
-        <div className={classes.header}>
-            Header
-        </div>
-    )
-}
 
-export default Header
+    const handleOpenMenu = () => {
+        const resOverlay = document.getElementById("res-overlay");
+        const openMenu = document.getElementById('open-menu')
+        const closeMenu = document.getElementById('close-menu')
+        resOverlay.style.display = "block";
+        openMenu.style.display = 'none'
+        closeMenu.style.display = 'block'
+    };
+
+    const handleCloseMenu = () => {
+        const resOverlay = document.getElementById("res-overlay");
+        const openMenu = document.getElementById('open-menu')
+        const closeMenu = document.getElementById('close-menu')
+        resOverlay.style.display = "none";
+        openMenu.style.display = 'block'
+        closeMenu.style.display = 'none'
+    }
+
+    return (
+        <>
+            <div className={classes.header}>
+                <div className={classes.topLeftHeader}>
+                    <div>
+                        <ApartmentIcon className={classes.topLeftIcons} /> 20
+                        Jerusalem Passage, London EC1V 4JP
+                    </div>
+                    <div>
+                        <PhoneIcon className={classes.topLeftIcons} /> 020 7126
+                        8370
+                    </div>
+                    <div>
+                        <EmailIcon className={classes.topLeftIcons} />{" "}
+                        info@wpagency.london
+                    </div>
+                </div>
+
+                <div className={classes.topRightHeader}>
+                    <div>
+                        <FacebookIcon className={classes.topRightIcons} />
+                    </div>
+                    <div>
+                        <TwitterIcon className={classes.topRightIcons} />
+                    </div>
+                    <div>
+                        <LinkedInIcon className={classes.topRightIcons} />
+                    </div>
+                </div>
+            </div>
+
+            <div className={classes.navbar}>
+                <img
+                    src={HeaderLogo}
+                    alt="header-logo"
+                    className={classes.logoImg}
+                />
+
+                <div>
+                    <div className={classes.headerMenuIconWrap}>
+                        <MenuIcon id="open-menu" className={classes.headerMenuIcon} onClick={handleOpenMenu}/>
+                        <CloseIcon id="close-menu" className={classes.headerMenuCloseIcon} onClick={handleCloseMenu} />
+                    </div>
+
+                    <ul className={classes.headerResMenu} id="res-overlay">
+                        <li>about</li>
+                        <li>our work</li>
+                        <li>web design</li>
+                        <li className={classes.resWordPressNav}>
+                            <div>
+                                wordpress <AddIcon />
+                            </div>
+                            <ul className={classes.resSubNav}>
+                                <li>WordPress Development</li>
+                                <li>WordPress Hosting</li>
+                                <li>WordPress Plugins</li>
+                                <li>WordPress Audits</li>
+                            </ul>
+                        </li>
+                        <li className={classes.resWordPressNav}>
+                            <div>
+                                plugins <AddIcon />
+                            </div>
+                            <ul className={classes.resSubNav}>
+                                <li>WordPress API Integrations</li>
+                                <li>WordPress Multilingual</li>
+                                <li>WP Jobs Manager Development</li>
+                                <li>WordPress Booking Plugins</li>
+                                <li>Gravity Forms Development</li>
+                            </ul>
+                        </li>
+                        <li style={{ paddingBottom: '15px'}}>seo & ppc</li>
+                    </ul>
+                </div>
+
+                <ul className={classes.navList}>
+                    <li>about</li>
+                    <li>our work</li>
+                    <li>web design</li>
+                    <li className={classes.wordPressNav}>
+                        <div>
+                            wordpress <AddIcon />
+                        </div>
+                        <ul className={classes.subNavList}>
+                            <li>WordPress Development</li>
+                            <li>WordPress Hosting</li>
+                            <li>WordPress Plugins</li>
+                            <li>WordPress Audits</li>
+                        </ul>
+                    </li>
+                    <li className={classes.wordPressNav}>
+                        <div>
+                            plugins <AddIcon />
+                        </div>
+                        <ul className={classes.subNavList}>
+                            <li>WordPress API Integrations</li>
+                            <li>WordPress Multilingual</li>
+                            <li>WP Jobs Manager Development</li>
+                            <li>WordPress Booking Plugins</li>
+                            <li>Gravity Forms Development</li>
+                        </ul>
+                    </li>
+                    <li>seo & ppc</li>
+                </ul>
+
+                <div className={classes.buttonWrap}>
+                    <button className={classes.navButton}>get a quote</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
--- src/layout/header/styles.js
+++ src/layout/header/styles.js
@@ -2,15 +2,182 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
     header: {
-        color: 'green'
+        display: "flex",
+        justifyContent: "space-between",
+        background: "#0f1d32",
+        height: "45px",
+    },
+    topLeftHeader: {
+        display: "flex",
+        "& > div": {
+            padding: "0px 20px",
+            color: "#fff",
+            display: "flex",
+            alignItems: "center",
+        },
+    },
+    topLeftIcons: {
+        fontSize: "20px",
+        marginRight: "5px",
+        color: "#f64668",
+    },
+    topRightHeader: {
+        display: "flex",
+        alignItems: "center",
+        "& > div": {
+            padding: "10px",
+            color: "#f2f2f2",
+        },
+    },
+    topRightIcons: {
+        fontSize: "20px",
+    },
+
+    logoImg: {
+        width: "170px",
+    },
+
+    navbar: {
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "18px 10px",
+    },
+
+    navList: {
+        listStyle: "none",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        "& > li": {
+            padding: "15px 30px",
+            textTransform: "uppercase",
+            cursor: 'pointer',
+            "& > div": {
+                display: "flex",
+                alignItems: "center",
+            },
+        },
+    },
+
+    wordPressNav: {
+        "&:hover": {
+           "& $subNavList": {
+               display: 'block'
+           }
+        }
+    },
+
+    subNavList: {
+        position: "absolute",
+        background: "#f1f1f1",
+        listStyle: "none",
+        marginLeft: "-20px",
+        marginTop: "15px",
+        borderRadius: '5px',
+        minWidth: '200px',
+        display: 'none',
+        cursor: 'pointer',
+        "& > li": {
+            padding: "12px 20px",
+            textTransform: 'none',
+            cursor: 'pointer',
+            "&:hover": {
+                background: '#f64668',
+                color: '#fff',
+                cursor: 'pointer'
+            }
+        },
+    },
+
+    buttonWrap: {
+        display: "flex",
+        alignItems: "center",
+    },
+
+    navButton: {
+        borderRadius: "30px",
+        padding: "10px 15px",
+        border: "none",
+        outline: "none",
+        background: "#f64668",
+        color: "#f2f2f2",
+        fontWeight: "bold",
+        height: "fit-content",
+        cursor: "pointer",
+        textTransform: "uppercase",
+    },
+
+    headerMenuIconWrap: {
+        marginTop: '15px'
+    },
+
+    headerMenuIcon: {
+        display: 'none'
+    },
+
+    headerMenuCloseIcon: {
+        display: 'none'
+    },
+
+    headerResMenu: {
+        position: 'absolute',
+        background: '#f2f2f2',
+        listStyle: 'none',
+        marginLeft: '-200px',
+        minWidth: '250px',
+        textTransform: 'uppercase',
+        borderRadius: '5px',
+        display: 'none',
+        "& > li": {
+            paddingTop: '10px',
+            paddingLeft: '15px'
+        },
+        "& > li > div": {
+            display: 'flex',
+            alignItems: 'center'
+        }
+    },
+
+    resSubNav: {
+        listStyle: 'none',
+        textTransform: 'none',
+        display: 'none',
+        "& > li": {
+            padding: '5px 5px 5px 0'
+        }
+    },
+
+    resWordPressNav: {
+        "&:hover": {
+            "& $resSubNav": {
+                display: 'block'
+            }
+        }
     },
 
     // Responsive Design
-    [theme.breakpoints.down('md')]: {
-        // do something
+    [theme.breakpoints.down("md")]: {
+        navList: {
+            display: 'none'
+        },
+        headerMenuIcon: {
+            display: 'block'
+        }
     },
-    [theme.breakpoints.down('xs')]: {
-       // do something
+    [theme.breakpoints.down('sm')]: {
+        topLeftHeader: {
+            "& > div": {
+                padding: '5px 7px'
+            }
+        },
+        topRightHeader: {
+            "& > div": {
+                padding: '5px'
+            }
+        }
+    },
+    [theme.breakpoints.down("xs")]: {
+        // do something
     },
 }));
 
