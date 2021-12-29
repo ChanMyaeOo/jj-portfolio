b5069cda912622485a59da2c8761d276d10f9e6f
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-12-29 10:42:08 +0630

    added get in touch section

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
+import EmailIcon from '@material-ui/icons/Email';
 import useStyles from './styles'
 
 const Footer = () => {
     const classes = useStyles();
     return (
-        <div className={classes.footer}>
-            Footer
+        <div>
+        <div className={classes.footerTop}>
+            <div className={classes.touchWrap}>
+                <h3 className={classes.touchHeader}>Get In Touch</h3>
+                <p className={classes.touchContact}>Contact us to discuss your requirement and receive a quotation for your WordPress web design and development project. </p>
+                <h3 className={classes.addressHeader}>Address</h3>
+                <h4 className={classes.addressName}>WP Agency London</h4>
+                <p className={classes.addressContact}>20 Jerusalem Passage, London EC1V 4JP</p>
+                <h3 className={classes.phone}>Phone Number</h3>
+                <div className={classes.phoneWrap}>
+                   <PhoneAndroidIcon className={classes.phoneIcon} />
+                   <span className={classes.phoneSpan}>020 7126 8370</span> 
+                </div>
+
+                <h3 className={classes.email}>Email</h3>
+                <div className={classes.emailWrap}>
+                    <EmailIcon className={classes.emailIcon} />
+                    <span className={classes.emailSpan}>info@wpagency.london</span>
+                </div>
+            </div>
+
+            <div className={classes.quoteWrap}>
+                <h3 className={classes.quote}>Request a Quote</h3>
+                <p className={classes.quoteContact}>Please leave details about your WordPress design and development project and we will contact you shortly.</p>
+                <form className={classes.form}>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email"/>
+                    <input type="text" placeholder="Phone" />
+                    <textarea placeholder="Message" className={classes.quoteArea}>
+
+                    </textarea>
+                    <input type="submit" value="SEND REQUEST" className={classes.quoteBtn} />
+                </form>
+            </div>
+        </div>
         </div>
     )
 }
--- src/layout/footer/styles.js
+++ src/layout/footer/styles.js
@@ -1,14 +1,127 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    footer: {
-        color: 'green'
+    footerTop: {
+        backgroundColor: '#f4f4f4',
+        display: 'flex',
+        padding: '60px 25px',
+    },
+    touchWrap: {
+        padding: '15px'
+    },
+    touchHeader: {
+        fontSize: '36px',
+        color: '#212236',
+        marginBottom: '25px'
+    },
+    touchContact: {
+        fontSize: '18px',
+        color: '#2a3950',
+        lineHeight: '1.5rem',
+        marginBottom: '20px'
+    },
+    addressHeader: {
+        fontSize: '22px',
+        color: '#212236',
+        marginBottom: '10px'
+    },
+    addressName: {
+        fontSize: '17px',
+        color: '#2a3950'
+    },
+    addressContact: {
+        fontSize: '17px',
+        lineHeight: '1.5rem',
+        color: '#2a3950',
+        marginBottom: '20px'
+    },
+    phone: {
+        fontSize: '22px',
+        color: '#212236',
+        marginBottom: '5px'
+    },
+    phoneWrap: {
+        marginBottom: '20px',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    email: {
+        fontSize: '22px',
+        color: '#212236',
+        marginBottom: '5px'
+    },
+    emailWrap: {
+        marginBottom: '20px',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    phoneSpan: {
+        color: '#f64668',
+        fontSize: '17px'
+    },
+    emailSpan: {
+        color: '#f64668',
+        fontSize: '17px'
+    },
+    phoneIcon: {
+        color: '#2a3950',
+        marginRight: '5px'
+    },
+    emailIcon: {
+        color: '#2a3950',
+        marginRight: '5px'
+    },  
+    quote: {
+        fontSize: '28px',
+        color: '#212236'
+    },
+    quoteContact: {
+        fontSize: '18px',
+        color: '#2a3950',
+        lineHeight: '1.5rem',
+        margin: '20px 0'
+    }, 
+    quoteWrap: {
+        backgroundColor: '#fff',
+        boxShadoe: '10px 20px 30px 1px rgb(0 0 0 / 10%)',
+        padding: '40px',
+        borderRadius: '15px',
+    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        "& > input": {
+            height: '42px',
+            margin: '7px 0',
+            padding: '5px',
+        }
+    },
+    quoteArea: {
+        margin: '7px 0',
+        minHeight: '70px',
+        minWidth: '100%',
+        maxWidth: '540px',
+        padding: '5px',
+        marginBottom: '20px'
     },
 
-
+    quoteBtn: {
+        background: '#f64668',
+        border: 'none',
+        outline: 'none',
+        color: '#fff',
+        fontWeight: 'bold',
+        cursor: 'pointer',
+        fontSize: '15px',
+    },
     // Responsive Design
-    [theme.breakpoints.down('md')]: {
-        // do something
+    [theme.breakpoints.down('sm')]: {
+        footerTop: {
+            flexDirection: 'column'
+        },
+        touchHeader: {
+            fontSize: '30px'
+        }
     },
     [theme.breakpoints.down('xs')]: {
        // do something
