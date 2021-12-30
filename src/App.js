c27a98c56bdec47e44e21ba6aeca5bd5151667dc
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-12-30 10:49:50 +0630

    added portfolio page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ScrollToTop from './utils/ScrollToTop'
 import Layout from './layout/Layout'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
+import Portfolio from './pages/portfolio/Portfolio'
 import './style.css'
 
 const App = () => {
@@ -16,6 +17,11 @@ const App = () => {
             <Router>
                 <ScrollToTop />
                 <Switch>
+                    <Route path="/portfolio">
+                        <Layout>
+                            <Portfolio />
+                        </Layout>
+                    </Route>
                     <Route path="/about">
                         <Layout>
                             <About />
---
+++ src/pages/portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageTitle from '../../components/page-title/PageTitle'
+import Works from '../../components/works/Works'
+
+const Portfolio = () => {
+    return (
+        <div>
+            <PageTitle title="WordPress Portfolio"/>
+            <Works />
+        </div>
+    )
+}
+
+export default Portfolio
